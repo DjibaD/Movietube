f1aa363bc27617ca6e61717026870368d2a90132
djibadiawara <djiba.diawara97@gmail.com>
2021-12-10 12:10:24 -0500

    The API is fetch

--- index.js
+++ index.js
@@ -28,5 +28,55 @@ const searchText = document.querySelector("#search-text")
 const searchedMovie = searchText.querySelector("#form-p")
 const genreResults = document.querySelector("#genre-results")
 
+// When header is clicked, go back one page 
 
+header.addEventListener('click', () => window.history.go(-1))
 
+// search by genre 
+genreBtn.addEventListener('click', (e) =>{
+    e.preventDefault()
+    searchedMovie.innerHTML = `Showing results for: ${inputValue.value}`;
+    
+    fetch(genreURL)
+    .then(res=> res.json())
+    .then(genres =>{
+        genreResults.innerHTML = " ";
+        genres.genres.forEach(genre => {
+        // Check if input is a valid genre 
+        if(inputValue.value === genre.name){
+            fetch(popularURL)
+            .then(res =>res.json())
+            .then(movies =>{
+                genreResults.innerHTML = " ";
+                movies.results.forEach(movie => {
+                    // For each movie check if the genre_ids array include the searched genre id
+                    const genreMatch = movie.genre_ids.includes(genre.id);
+                    // If match found(true), create elements and add to the genre-results div
+                    if(genreMatch){
+                        console.log(movie.title)
+                        const movieResult = document.createElement('div')
+                        const movieImage = document.createElement('img')
+                        const movieTitle = document.createElement('span')
+                        const likeBtn = document.createElement('button')
+                        movieResult.classList.add('each-movie');
+                        // If no poster, don't assign/display the movie 
+                        if(movie.poster_path !== null){
+                            movieTitle.innerText = movie.title;
+                            movieTitle.classList.add('title');
+                            movieImage.src = imgPath + movie.poster_path;
+                            likeBtn.textContent = "♡";
+                            movieResult.append(movieImage, movieTitle, likeBtn);
+                            genreResults.append(movieResult);
+                        }
+                        // Add functionality to the like button
+                        likeBtn.addEventListener('click', () =>{
+                            likeBtn.textContent = "♥️";
+                            likedMovies.style.dispaly = "inline-block";
+                        })
+                    }     
+                })
+            })
+        }     
+        }); 
+    })
+})
