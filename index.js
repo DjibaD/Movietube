61f08e119c8f2aacb783e538330b173a4ad3a683
djibadiawara <djiba.diawara97@gmail.com>
2021-12-10 11:16:57 -0500

    The elements are assign to the variable

--- index.js
+++ index.js
@@ -1,126 +1,32 @@
-const API_KEY = 'api_key=a5035759f1fe71a6fd596a87600d36d2';
-const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
-const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-const searchURL = BASE_URL + '/search/movie?'+API_KEY;
-const main = document.getElementById('main')
-const form = document.getElementById('form')
-const search = document.getElementById('search')
+const API_KEY = 'a5035759f1fe71a6fd596a87600d36d2';
+const apiURL = 'https://api.themoviedb.org/3/search/movie?api_key=88449d803a568a37169bac400f61bdee'
+const imgPath = 'https://image.tmdb.org/t/p/w1280'
+const popularURL = "https://api.themoviedb.org/3/movie/popular?api_key=88449d803a568a37169bac400f61bdee"
+const upcomingURL = "https://api.themoviedb.org/3/movie/upcoming?api_key=88449d803a568a37169bac400f61bdee"
+const genreURL = "https://api.themoviedb.org/3/genre/movie/list?api_key=88449d803a568a37169bac400f61bdee"
 
-//Set the different Genres
-const genres = [
-    {
-      "id": 28,
-      "name": "Action"
-    },
-    {
-      "id": 12,
-      "name": "Adventure"
-    },
-    {
-      "id": 16,
-      "name": "Animation"
-    },
-    {
-      "id": 35,
-      "name": "Comedy"
-    },
-    {
-      "id": 80,
-      "name": "Crime"
-    },
-    {
-      "id": 99,
-      "name": "Documentary"
-    },
-    {
-      "id": 18,
-      "name": "Drama"
-    },
-    {
-      "id": 10751,
-      "name": "Family"
-    },
-    {
-      "id": 14,
-      "name": "Fantasy"
-    },
-    {
-      "id": 36,
-      "name": "History"
-    },
-    {
-      "id": 27,
-      "name": "Horror"
-    },
-    {
-      "id": 10402,
-      "name": "Music"
-    },
-    {
-      "id": 9648,
-      "name": "Mystery"
-    },
-    {
-      "id": 10749,
-      "name": "Romance"
-    },
-    {
-      "id": 878,
-      "name": "Science Fiction"
-    },
-    {
-      "id": 10770,
-      "name": "TV Movie"
-    },
-    {
-      "id": 53,
-      "name": "Thriller"
-    },
-    {
-      "id": 10752,
-      "name": "War"
-    },
-    {
-      "id": 37,
-      "name": "Western"
-    }
-  ]
-    
+//grab the elements and assign a variable 
 
-// Get the movies data
-getMovies(API_URL);
+const inputValue = document.querySelector('#inputValue');  
+const searchBtn = document.querySelector('#searchBtn')
+const genreBtn = document.querySelector("#genreBtn")
+const searchResults = document.querySelector('#search-results')
+const form = document.querySelector('#search-form')
+const modal = document.querySelector("#modal")
+const displayTitle = modal.querySelector("#detail-title")  
+const displayImage = modal.querySelector(".detail-img")     
+const overView = modal.querySelector("#overview")
+const votes = modal.querySelector("#votes")
+const trailer = modal.querySelector("trailer")
+const releaseDate = modal.querySelector("#release_date")
+const closeBtn = document.querySelector('#close')
+const popularMovies = document.querySelector('#popular-movies')
+const upcomingMovies = document.querySelector('#upcoming-movies')
+const likedMovies = document.querySelector("#liked-movies");
+const header = document.querySelector('h1')
+const searchText = document.querySelector("#search-text")
+const searchedMovie = searchText.querySelector("#form-p")
+const genreResults = document.querySelector("#genre-results")
 
-function getMovies(url) {
-  lastUrl = url;
-    fetch(url).then(res => res.json()).then(data => {
-        console.log(data.results)
-        if(data.results.length !== 0){
-            showMovies(data.results);
-            currentPage = data.page;
-            nextPage = currentPage + 1;
-            prevPage = currentPage - 1;
-            totalPages = data.total_pages;
 
-            current.innerText = currentPage;
 
-            if(currentPage <= 1){
-              prev.classList.add('disabled');
-              next.classList.remove('disabled')
-            }else if(currentPage>= totalPages){
-              prev.classList.remove('disabled');
-              next.classList.add('disabled')
-            }else{
-              prev.classList.remove('disabled');
-              next.classList.remove('disabled')
-            }
-
-            // tagsEl.scrollIntoView({behavior : 'smooth'})
-
-        }else{
-            main.innerHTML= `<h1 class="no-results">No Results Found</h1>`
-        }
-       
-    })
-
-}
